87bd471ca0ebfe3b45e38f50fad2648a20a46be0
Shivam <goelshivam2012@gmail.com>
2023-09-28 13:25:40 +0530

    first commit

--- src/main/java/com/example/Hyper/Market/HyperMarketApplication.java
+++ src/main/java/com/example/Hyper/Market/HyperMarketApplication.java
@@ -2,9 +2,20 @@ package com.example.Hyper.Market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ImportResource
+@RestController
 public class HyperMarketApplication {
+
+	@GetMapping("/")
+	public String home(){
+				// received this message in aws console
+		return "First Deployed Project... !!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HyperMarketApplication.class, args);
